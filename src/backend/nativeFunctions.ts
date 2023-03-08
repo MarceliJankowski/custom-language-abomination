4dbfe671adf1e7b932599ab2e9a6bcb74e87ddf9
Marceli Jankowski <marcelijankowski123@gmail.com>
2023-03-08 01:23:18 +0100

    updating native function implementations

--- src/backend/nativeFunctions.ts
+++ src/backend/nativeFunctions.ts
@@ -10,6 +10,7 @@ import * as MK from "./runtimeValueFactories";
 //               NATIVE FUNCTIONS
 // -----------------------------------------------
 
+/**@desc log `arguments` to std output in a `non-verbose` way*/
 export const log = MK.NATIVE_FUNCTION(args => {
   const parsedArgs = args.map(arg => parseForLogging(arg));
 
@@ -18,30 +19,44 @@ export const log = MK.NATIVE_FUNCTION(args => {
   return MK.UNDEFINED();
 });
 
+/**@desc log `arguments` to std output in a `verbose` way*/
 export const logVerbose = MK.NATIVE_FUNCTION(args => {
   console.log(...args);
 
   return MK.UNDEFINED();
 });
 
+/**@desc clear the terminal/console*/
 export const clear = MK.NATIVE_FUNCTION(() => {
   console.clear();
 
   return MK.UNDEFINED();
 });
 
+/**@desc terminate process with `exitCode` 
+@param exitCode integer in range of `0-255`*/
 export const exit = MK.NATIVE_FUNCTION(([firstArg]) => {
-  if (firstArg && firstArg.type !== "number")
-    throw new Err(
-      `Invalid errCode argument type: '${firstArg.type}' passed to 'exit' native function`,
-      "interpreter"
-    );
+  if (firstArg !== undefined) {
+    if (firstArg.type !== "number")
+      throw new Err(
+        `Invalid exitCode type: '${firstArg.type}' passed to 'exit()' native function`,
+        "interpreter"
+      );
+
+    if (!isValidExitCode(firstArg.value as number))
+      throw new Err(
+        `Invalid exitCode argument: '${firstArg.value}' (valid range: 0-255) passed to 'exit()' native function`,
+        "interpreter"
+      );
+  }
 
   const exitCode = (firstArg?.value as number) ?? 0;
 
   process.exit(exitCode);
 });
 
+/**@desc prompt user for input
+@param message string preceding input prompt. If message isn't provided it defaults to empty string*/
 export const prompt = MK.NATIVE_FUNCTION(([firstArg]) => {
   if (firstArg && firstArg.type !== "string")
     throw new Err(
@@ -55,3 +70,12 @@ export const prompt = MK.NATIVE_FUNCTION(([firstArg]) => {
   const output = MK.STRING(userInput);
   return output;
 });
+
+// -----------------------------------------------
+//                    UTILS
+// -----------------------------------------------
+
+/**@desc determine whether `exitCode` is valid (in range: `0-255`)*/
+function isValidExitCode(exitCode: number) {
+  return exitCode >= 0 && exitCode <= 255;
+}
